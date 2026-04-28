c8210a608a82d1c3c95f6a720d517632d1c139dc
De_ice47 <kasprzyk.tim@protonmail.com>
2026-04-27 19:36:04 -0500

    Update Settings.cpp

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -4,8 +4,10 @@ namespace ArduinoLinkPLC
 {
     // Device Settings
     const char *deviceSenderID = "ArdUnoR4";
-    IPAddress masterComputerIP(192, 168, 137, 1);        // Input the master device
-    const char *masterComputerSSID = "ISR30OK 3700";    // Master device network name
-    const char *masterComputerSSIDPassword = "47x8%B28"; // Master device network password
-    const int masterComputerPort = 49500;                 // Master device port
+    IPAddress masterComputerIP(0, 0, 0, 0);        // Input the master device
+    const char *masterComputerSSID = "";    // Master device network name
+    const char *masterComputerSSIDPassword = ""; // Master device network password
+    const int masterComputerPort = 00000;                 // Master device port
+
+    
 }
